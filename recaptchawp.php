195e6d6a14b783960fd28662eb08051c6b6f84c7
Ibronos <33568735+ibronos@users.noreply.github.com>
2024-09-11 16:58:09 +0700

    y

--- recaptchawp.php
+++ recaptchawp.php
@@ -13,7 +13,7 @@
  * Version:     1.0.0
  * Author:      isaidi
  * Author URI:  https://isaidi.vercel.app
- * Text Domain: Captcha Plugin
+ * Text Domain: recaptchawp
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
